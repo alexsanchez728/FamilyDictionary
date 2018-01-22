d031d45a98a2fc93792a5573742e7683f0bd9ba8
Alex Sanchez <alesanchez728@gmail.com>
2018-01-22 14:14:46 -0600

    works

--- FamilyDictionary/Program.cs
+++ FamilyDictionary/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace FamilyDictionary
 {
@@ -10,6 +7,31 @@ namespace FamilyDictionary
     {
         static void Main(string[] args)
         {
+            Dictionary<string, Dictionary<string, string>> myFamily = new Dictionary<string, Dictionary<string, string>>();
+
+            myFamily.Add("sister", new Dictionary<string, string>(){ 
+                {"name", "Krista"},
+                {"age", "42"}
+            });
+
+            foreach (var person in myFamily)
+                // goes over each family member, in this dictionary there is only 1 family member...
+                // here, person is the dictionary of Key: "sister" and value: new Dictionary
+            {
+                var relation = person.Key; // here, "sister"
+
+                var personName = myFamily[relation]["name"];
+                //              ^ opens up the myFamily dictionary with the key of "sister"...
+                //                  in "sister" dictionary the key of "name" is used to reach the value: "Krista"
+
+                var personAge = myFamily[relation]["age"];
+                //              ^ opens up the myFamily dictionary with the key of "sister"...
+                //                  in "sister" dictionary the key of "age" is used to reach the value: "42"
+
+                Console.WriteLine($"{personName} is my {relation} and is {personAge} years old.");
+            }
+
+            Console.ReadKey();
         }
     }
 }
